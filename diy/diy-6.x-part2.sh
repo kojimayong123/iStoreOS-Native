6c2c949102b604ff6819171c110646a64e95a67a
kojimayong123 <131628117+kojimayong123@users.noreply.github.com>
2025-12-10 10:40:10 +0800

    Replace tvi3315a with am40 device configuration

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -14,28 +14,28 @@ cp -a $GITHUB_WORKSPACE/configfiles/firmware/brcm/* package/base-files/files/lib
 # ================================================================
 # 移植RK3399示例，其他RK3399可模仿
 # ================================================================
-# 增加tvi3315a设备
-echo -e "\\ndefine Device/tvi_tvi3315a
-  DEVICE_VENDOR := Tvi
-  DEVICE_MODEL := TVI3315A
+# 增加am40设备
+echo -e "\\ndefine Device/smart_am40
+  DEVICE_VENDOR := SMART
+  DEVICE_MODEL := AM40
   SOC := rk3399
-  UBOOT_DEVICE_NAME := tvi3315a-rk3399
+  UBOOT_DEVICE_NAME := am40-rk3399
 endef
-TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
+TARGET_DEVICES += smart_am40" >> target/linux/rockchip/image/armv8.mk
 
 # 替换package/boot/uboot-rockchip/Makefile
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
 
 # 复制dts与配置文件到package/boot/uboot-rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-am40.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-am40-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/am40-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
 
 # 复制dts到files/arch/arm64/boot/dts/rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-am40.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 
 # 添加dtb补丁到target/linux/rockchip/patches-6.6
-cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
+cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-am40-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
 # ================================================================
 # RK3399示例结束
 # ================================================================
