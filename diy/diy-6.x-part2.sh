fe234ca02e87919b8b7d95465f9be6f37db44551
kojimayong123 <131628117+kojimayong123@users.noreply.github.com>
2025-12-23 21:12:57 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -23,11 +23,12 @@ echo -e "\\ndefine Device/smart_am40
   DEVICE_PACKAGES := wpad-mbedtls kmod-rtw88-8822be kmod-bluetooth kmod-usb-dwc3 \
   kmod-sound-soc-simple-card kmod-sound-soc-rockchip kmod-sound-soc-hdmi-codec \
   kmod-drm-rockchip kmod-drm-panfrost kmod-extcon-usbc-virtual-pd rockchip-cdn-dp-firmware
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_DTS = rk3399/rk3399-am40
+
   
 endef
 TARGET_DEVICES += smart_am40" >> target/linux/rockchip/image/armv8.mk
+# DEVICE_DTS_DIR := ../dts
+# DEVICE_DTS = rk3399/rk3399-am40
 # KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-rk3399-am40.dtb
 # 替换package/boot/uboot-rockchip/Makefile
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
